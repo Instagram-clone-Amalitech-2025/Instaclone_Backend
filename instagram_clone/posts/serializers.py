98f66febc4326881ad145d42da3439504897c515
King-khid <kingkhid60@gmail.com>
2025-05-08 15:34:03 -0700

    modifications made on serializers

--- instagram_clone/posts/serializers.py
+++ instagram_clone/posts/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')  # shows username, but user can't set it
+    user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
         model = Post
-        fields = ['id', 'user', 'image', 'caption', 'created_at']
+        fields = ['id', 'user', 'image', 'video', 'caption', 'created_at']
