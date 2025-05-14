892ac97cb95382589083e60c30b458e2678e122d
King-khid <kingkhid60@gmail.com>
2025-05-14 10:11:09 -0700

    added new feature to the authentication

--- src/instagram_clone/urls.py
+++ src/instagram_clone/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('account.urls'),),
+    path('auth/', include('user_account.urls'),),
     #urlpath for post
     path('posts/', include('posts.urls')),
     
